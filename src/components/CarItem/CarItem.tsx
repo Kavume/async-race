263917e985d92ebddae1df52d28066486a8abd81
Kavume <e.gorbacheva404@gmail.com>
2024-04-18 14:00:06 +0200

    feat: add styles for car item component

--- src/components/CarItem/CarItem.tsx
+++ src/components/CarItem/CarItem.tsx
@@ -1,5 +1,6 @@
 import { Button } from '../Button';
 import { CarIcon } from '../../assets/icons';
+import styles from './CarItem.module.scss';
 
 interface CarItemProps {
   carColor: string;
@@ -8,19 +9,21 @@ interface CarItemProps {
 
 function CarItem({ carColor, carName }: CarItemProps) {
   return (
-        <div>
-          <div>
-            <div>
+        <div className={styles.itemWrapper}>
+          <div className={styles.actionPanel}>
+            <div className={styles.actionControlBtns}>
               <Button text={'select'} size={'small'} color={'blue'} onClick={() => console.log('select button')} />
               <Button text={'remove'} size={'small'} color={'pink'} onClick={() => console.log('remove button')}/>
             </div>
-            <div>
+            <div className={styles.engineControlBtns}>
               <Button text={'a'} size={'small'} color={'yellow'} onClick={() => console.log('start engine')}/>
               <Button text={'b'} size={'small'} color={'disable'} onClick={() => console.log('stop engine')}/>
             </div>
+              <div className={styles.icon}>
+                  <CarIcon colorIcon={carColor} />
+              </div>
           </div>
-          <div>{carName}</div>
-          <CarIcon colorIcon={carColor} />
+          <p className={styles.text}>{carName}</p>
         </div>
   );
 }
